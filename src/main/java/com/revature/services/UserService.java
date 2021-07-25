c064fa21da8cc98678d9fbaad88468d5c545e1c8
June Greenup <cgreenup2000@gmail.com>
2021-07-24 19:34:53 -0500

    Added SQL database table creation script and Started JDBC

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -3,6 +3,11 @@ package com.revature.services;
 public class UserService {
 
 	public boolean login(String username, String password) {
+		//connect to database
+		//Get username
+		//Get encrypted password
+		
+		//Compare to 
 		
 		return false;
 	}
---
+++ src/main/java/com/revature/utils/Connectionutil.java
@@ -0,0 +1,25 @@
+package com.revature.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connectionutil {
+
+	public static Connection getConnection() throws SQLException {
+		
+		 try {
+			Class.forName("org.postgresql.Driver");
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		String url = "jdbc:postgresql://training-db.cfhaaft3h8hf.us-east-2.rds.amazonaws.com:5432/eri";
+		String username = "postgres";
+		String password = "password";
+		
+		return DriverManager.getConnection(url, username, password);
+		
+	}
+	
+}
