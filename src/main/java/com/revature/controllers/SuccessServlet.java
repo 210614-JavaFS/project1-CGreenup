3ca2716d2566db3ab9079f6ab844982cc3f48725
June Greenup <cgreenup2000@gmail.com>
2021-07-25 23:08:11 -0500

    Added Employee screen

--- src/main/java/com/revature/controllers/SuccessServlet.java
+++ src/main/java/com/revature/controllers/SuccessServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.revature.models.User;
+import com.revature.models.UserRoles;
 import com.revature.services.UserService;
 
 public class SuccessServlet extends HttpServlet{
@@ -20,6 +21,11 @@ public class SuccessServlet extends HttpServlet{
 		resp.setContentType("text/html");
 		PrintWriter pw = resp.getWriter();
 		
+		pw.print("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" "
+				+ "rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" "
+				+ "crossorigin=\"anonymous\">"
+				+ "<link rel=\"stylesheet\" href=\"employee.css\">");
+		
 		if(session == null) {
 			pw.print("<div class = \"alert alert-danger\">"
 					+ "<strong>PLEASE RETURN TO HOME PAGE</strong>"
@@ -28,7 +34,14 @@ public class SuccessServlet extends HttpServlet{
 			//DEBUG
 			UserService.getUserService();
 			User user = UserService.getUser((String) session.getAttribute("username"));
-			pw.print("<h2>Welcome " + user.getFirstName() + ".");
+
+			if(user.getUserType() == UserRoles.MANAGER) {
+				pw.print("<div><p>Manager</p>");
+				pw.print("<p>Second ptag</p></div>");
+			}else {
+				displayEmployeeScreen(pw);
+			}
+			
 		}
 	}
 	
@@ -36,4 +49,50 @@ public class SuccessServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req, resp);
 	}
+	
+	protected void displayEmployeeScreen(PrintWriter pw) {
+		pw.print("<style>"
+				+ "#mainDiv{\r\n"
+				+ "    margin: 20px;\r\n"
+				+ "  }\r\n"
+				+ "  \r\n"
+				+ "  button{\r\n"
+				+ "    width:100%;\r\n"
+				+ "    height:70px;\r\n"
+				+ "  }\r\n"
+				+ "  \r\n"
+				+ "  #tableHead{\r\n"
+				+ "    background: black;\r\n"
+				+ "    color: white;\r\n"
+				+ "  }\r\n"
+				+ "  \r\n"
+				+ "  label{\r\n"
+				+ "    padding-bottom: 5px;\r\n"
+				+ "    padding-left: 8px;\r\n"
+				+ "  }"
+				+ "</style>");
+		
+		pw.print("<div id='mainDiv'>\r\n"
+				+ "  <button class = \"btn btn-outline-secondary\" >Create New Request</button>\r\n"
+				+ "  \r\n"
+				+ "  <div id='tableDiv'>\r\n"
+				+ "    <label for='requestTable'><b>Submitted Requests</b></label>\r\n"
+				+ "  <table class=\"table\" id='requestTable'>\r\n"
+				+ "    <thead id='tableHead'>\r\n"
+				+ "      <tr>\r\n"
+				+ "        <th scope='col'> ID </th>\r\n"
+				+ "        <th scope='col'> Description </th>\r\n"
+				+ "        <th scope='col'> Amount </th>\r\n"
+				+ "        <th scope='col'> Status </th>\r\n"
+				+ "      </tr>\r\n"
+				+ "    </thead>\r\n"
+				+ "    \r\n"
+				+ "    <tbody>\r\n"
+				+ "      \r\n"
+				+ "    </tbody>\r\n"
+				+ "  </table>\r\n"
+				+ "  </div>\r\n"
+				+ "</div>");
+	}
+	
 }
