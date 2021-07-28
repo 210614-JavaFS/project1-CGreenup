ca740d74e6cb8b28cadd5ae4e055c8e159e86056
June Greenup <cgreenup2000@gmail.com>
2021-07-28 01:33:29 -0500

    javascript works, tomcat is just completely blind to the javascript

--- src/main/webapp/printSubmissionForm.js
+++ src/main/webapp/printSubmissionForm.js
@@ -7,7 +7,7 @@ formDiv.id = 'formDiv'
 //Form
 let reimbForm = document.createElement('form');
 reimbForm.id = 'signUpForm';
-reimbForm.action = 'new-request';
+//reimbForm.action = 'new-request';
 reimbForm.method = 'post';
 
 //Select Label
@@ -41,6 +41,7 @@ input.id = 'dollarAmount';
 input.type = 'text';
 input.className = 'form-control';
 input.placeholder = '000.00';
+input.required = 'true';
 formDiv.appendChild(input);
 
 //Description Label
@@ -48,6 +49,60 @@ label = document.createElement('label');
 label.innerHTML = 'A short description for the request';
 formDiv.appendChild(label);
 
+//Description input
+input = document.createElement('input');
+input.type = 'text';
+input.id = 'description';
+input.className = 'form-control';
+input.placeholder = 'Description';
+input.required = 'true';
+formDiv.appendChild(input);
 
+//Whitespace - not as high as a line break
+label = document.createElement('label');
+formDiv.appendChild(label);
 
-mainDiv.appendChild(formDiv);
+let button = document.createElement('button');
+button.id = 'okButton';
+button.className = 'btn form-control btn-secondary';
+button.disabled = 'true';
+button.innerHTML = 'Submit';
+formDiv.appendChild(button);
+
+function showForm(){
+    mainDiv.appendChild(formDiv);
+}
+
+function clearAll(username) {
+    var div = document.getElementById('mainDiv');
+    while(div.firstChild){
+        div.removeChild(div.firstChild);
+    }
+    showForm();
+    setUser(username);
+}
+
+const inputRegex = /^\d+(\.\d{1,2})?$/;
+  const signUpForm = document.getElementById('signUpForm');
+  const dollarAmount = document.getElementById('dollarAmount')
+  const descriptionField = document.getElementById('description');
+  const okButton = document.getElementById('okButton');
+  let user;
+
+  descriptionField.addEventListener('keyup', validateForm);
+  dollarAmount.addEventListener('keyup', validateForm);
+  
+    function setUser(username){
+        user = username;
+    }
+
+  function validateForm() {
+    descriptionNotEmpty = descriptionField.checkValidity();
+    dollarAmountIsValid = inputRegex.test(dollarAmount.value)
+
+    if ( descriptionNotEmpty && dollarAmountIsValid) {
+      okButton.disabled = false;
+    } else {
+      okButton.disabled = true;
+    }
+  }
